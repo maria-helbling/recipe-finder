19f5182d427cfa8d8c14c636dbb5103fed695139
Bryce Pingul <brycepingul@gmail.com>
2020-06-23 14:39:27 -0700

    able to capture current country cases

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-var userCountry = $("country-choice");
-var queryURL = "https://api.covid19api.com/country/" + "china" + "/status/confirmed/live?from=" + "2020-06-01T00:00:00Z" + "&to=" + "2020-06-22T00:00:00Z";
+// var userCountry = $("country-choice");
+var queryURL = "https://api.covid19api.com/live/country/italy/status/confirmed/date/2020-03-21T13:13:30Z";
+
+console.log(queryURL)
 
 $.ajax ({
     url: queryURL,
     method: "GET"
 }) .then(function(response){
     console.log(response);
-})
+    var currentCases = response[response.length - 1].Active;
+    console.log(currentCases);
+});
