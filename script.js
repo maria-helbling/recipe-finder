cee0eaecc9f4468a4cd14c1f79ec3c62640e0e1d
mahelb <65257133+maria-helbling@users.noreply.github.com>
2020-06-24 13:58:08 -0700

    event listener and results rendering working

--- script.js
+++ script.js
@@ -1,58 +1,47 @@
 let countryChoice;
+let activeCases;
+let confirmedDiff;
 let currentPop;
-
-//listen to drop-down menu change
-$('#country-choice').change(function(){
-    countryChoice = $(this).val()
-    // this will go inside the countryData ajax: if (countryChoice === 'United States') {countryChoice='US'}
-    console.log(countryChoice)
-    //call ajax for case data
-    countryData();
-    //call ajax for population data
-    countryPop();
-    //TODO: call result display function
-})
 
 //populates countries to drop-down from countries.js
 populateCountries("country-choice");
 
-var countryName = $("#country-choice");
-
 // grabbing country covid data
-function countryData (){
-    var resultBox = $("#result-box");
-    
-    var queryURL = "https://covid-api.com/api/reports?date=" + "2020-06-23" + "&region_name=" + "estonia";
+function countryData() {
+    if (countryChoice === 'United States') {countryChoice='US'}
+    var queryURL = "https://covid-api.com/api/reports?date=" + "2020-06-23" + "&region_name=" + countryChoice;
 
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response){
-        var activeCases = 0;
+    }).then(function (response) {
+        activeCases = 0;
         // var confirmedCases = 0;
         confirmedDiff = 0;
-        var regionCount= response.data.length
+        var regionCount = response.data.length
         // for loop to grap the data for each region/province
         for (let i = 0; i < regionCount; i++) {
             activeCases += (response.data[i].active);
-            
+
             confirmedDiff += response.data[i].confirmed_diff;
         }
-        console.log(activeCases);
-        console.log(confirmedDiff)
+
+        renderResults();
     })
 }
+
 // grabbing country population for calculations
-function countryPop (){
-    var popURL = "https://data.opendatasoft.com/api/records/1.0/search/?dataset=world-population%40kapsarc&q=" + "Estonia" + "&facet=year&facet=country_name&refine.year=2018"
+function countryPop() {
+    var popURL = "https://data.opendatasoft.com/api/records/1.0/search/?dataset=world-population%40kapsarc&q=" + countryChoice + "&facet=year&facet=country_name&refine.year=2018"
 
     $.ajax({
         url: popURL,
         method: "GET"
-    }).then(function(response){
-        var currentPop = response.records[0].fields.value;
-        console.log(currentPop);
+    }).then(function (response) {
+        currentPop = response.records[0].fields.value;
+        countryData();
     })
+        
 }
 
 // function to calculate trend from array of numbers
@@ -63,16 +52,31 @@ let calcTrend = (numArr) => {
     let sumY = 0;
     let n = numArr.length;
     numArr.forEach((value, index) => {
-    sumX += index;
-    sumX2 += index**2;
-    sumXY += index*value;
-    sumY += value;     
+        sumX += index;
+        sumX2 += index ** 2;
+        sumXY += index * value;
+        sumY += value;
     })
-    let b=(sumXY - sumX*sumY/n)/(sumX2 - sumX*sumX/n);
+    let b = (sumXY - sumX * sumY / n) / (sumX2 - sumX * sumX / n);
     return b;
 }
 
 //render results on page
 let renderResults = () => {
     
+    let dispActive = (activeCases/currentPop)*100000
+    dispActive = dispActive.toFixed(1)
+    let dispNew = (confirmedDiff/currentPop)*100000
+    dispNew = dispNew.toFixed(2)
+    $('#result-box').append($('<div id="active-cases">').text(dispActive))
+    $('#result-box').append($('<div id="new-cases">').text(dispNew))
 }
+
+//listen to drop-down menu change
+$('#country-choice').change(function () {
+    countryChoice = $(this).val()
+    // this will go inside the countryData ajax: 
+    console.log(countryChoice)
+    //call ajax for population data ==> countryData ==> renderResults
+    countryPop();
+})
