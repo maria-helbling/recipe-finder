c6c678aaea664116efff7e918f99487c74bad2fd
mahelb <65257133+maria-helbling@users.noreply.github.com>
2020-06-24 13:09:55 -0700

    added event listener for drop-down

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let countryChoice;
+let currentPop;
+
+//listen to drop-down menu change
+$('#country-choice').change(function(){
+    countryChoice = $(this).val()
+    // this will go inside the countryData ajax: if (countryChoice === 'United States') {countryChoice='US'}
+    console.log(countryChoice)
+    //call ajax for case data
+    countryData();
+    //call ajax for population data
+    countryPop();
+    //TODO: call result display function
+})
+
 //populates countries to drop-down from countries.js
 populateCountries("country-choice");
 
@@ -16,4 +31,9 @@ let calcTrend = (numArr) => {
     })
     let b=(sumXY - sumX*sumY/n)/(sumX2 - sumX*sumX/n);
     return b;
+}
+
+//render results on page
+let renderResults = () => {
+    
 }
